e4ea5f1f739949f63c51d77e168fc3b9c6b786bf
WdnmdQzqz616 <albertjshederay@gmail.com>
2020-09-08 21:29:59 +0800

    compare the two recursion algo one is straightly recursion another memory search

--- Data_Structure/stack/catalan_stack.cpp
+++ Data_Structure/stack/catalan_stack.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
+/*
 #define MAXN 20
 
 int memo[MAXN];
@@ -22,14 +23,35 @@ int catalan(int n)
     memo[n] = sum;
     return sum;
 }
+*/
 
+int catalan(int n)
+{
+    if (!n) return 1;
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += catalan(i) * catalan(n - 1 - i);
+    }
+    return sum;
+}
 
 int main()
 {
+    /*
     memset(memo, 0, MAXN * sizeof(int));
     memo[0] = 1;
+    */
     int n;
     cin >> n;
     cout << catalan(n) << endl;
     return 0; 
 }
+
+/**
+ * 总结栈的可能序列
+ * 理解：考虑 1 在合法序列中的位置，在第 k 个位，前面 k-1 个数又合法性不会大于 k+1，后面有 n-1-k 个数
+ * 递推公式： f(n) = \sum^{n-1}_{i=0} f(i)f(n-1-i)
+ * 本质上是卡特兰数
+ * 使用递归采取记忆化搜索提高效率：根据评测结果：n = 18 差不多是不用记忆化不超时的极限，993ms，用记忆化 2ms
+ */
