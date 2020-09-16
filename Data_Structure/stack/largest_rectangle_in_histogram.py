0a09e15b03c38311ca1e781831f391d206b43906
WdnmdQzqz616 <albertjshederay@gmail.com>
2020-09-16 14:21:00 +0800

    Slightly explain the O(N^2) solution

--- Data_Structure/stack/largest_rectangle_in_histogram.py
+++ Data_Structure/stack/largest_rectangle_in_histogram.py
@@ -14,6 +14,7 @@ class Solution:
                 if heights[k] < heights[i]:
                     y = k
                     break
+            # 找到 height[i] 两侧第一个比自己矮的，下标为 x,y ，计算夹在中间的面积宽为 y - x- 1
             maxArea = max(maxArea, heights[i] * (y - x - 1))
         return maxArea
 
